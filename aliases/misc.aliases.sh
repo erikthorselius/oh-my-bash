23cda8a6a373a9585f3b953c69d3116dd9b7e6c4
Erik Thorselius <erik.thorselius@gmail.com>
2019-10-30 07:46:19 +0100

    Own changes

--- aliases/misc.aliases.sh
+++ aliases/misc.aliases.sh
@@ -64,9 +64,9 @@ alias qfind="find . -name "                 # qfind:    Quickly search for file
 #   ---------------------------
 
 alias netCons='lsof -i'                                   # netCons:      Show all open TCP/IP sockets
-alias lsock='sudo /usr/sbin/lsof -i -P'                   # lsock:        Display open sockets
-alias lsockU='sudo /usr/sbin/lsof -nP | grep UDP'         # lsockU:       Display only open UDP sockets
-alias lsockT='sudo /usr/sbin/lsof -nP | grep TCP'         # lsockT:       Display only open TCP sockets
+alias lsock='sudo /usr/bin/lsof -i -P'                   # lsock:        Display open sockets
+alias lsockU='sudo /usr/bin/lsof -nP | grep UDP'         # lsockU:       Display only open UDP sockets
+alias lsockT='sudo /usr/bin/lsof -nP | grep TCP'         # lsockT:       Display only open TCP sockets
 alias ipInfo0='ifconfig getpacket en0'                    # ipInfo0:      Get info on connections for en0
 alias ipInfo1='ifconfig getpacket en1'                    # ipInfo1:      Get info on connections for en1
 alias openPorts='sudo lsof -i | grep LISTEN'              # openPorts:    All listening connections
